acd67e8603fc4400c2745dbb9dae33efc061cbf7
Ayşe Erdem <aayseerdem0633@gmail.com>
2022-08-26 08:33:09 +0300

    25 Agustos

---
+++ team8.sql
@@ -0,0 +1,29 @@
+CREATE TABLE calisanlar
+(
+id CHAR(5), -- not null + unique
+isim VARCHAR(50) UNIQUE, -- UNIQUE --> Bir sutundaki tüm değerlerin BENZERSİZ yani tek olmasını sağlar
+maas int NOT NULL, -- NOT NULL --> Bir sutunun NULL içermemesini yani boş olmamasını sağlar
+ise_baslama DATE
+);
+select * from calisanlar;
+INSERT INTO calisanlar VALUES('10002', 'Mehmet Yılmaz' ,12000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10008', null, 5000, '2018-04-14');
+--INSERT INTO calisanlar VALUES('10010', Mehmet Yılmaz, 5000, '2018-04-14'); --Unique
+INSERT INTO calisanlar VALUES('10004', 'Veli Han', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10005', 'Mustafa Ali', 5000, '2018-04-14');
+--INSERT INTO calisanlar VALUES('10006', 'Canan Yaş', NULL, '2019-04-12'); --NOT NULL
+INSERT INTO calisanlar VALUES('10003', 'CAN', 5000, '2018-04-14');
+--INSERT INTO calisanlar VALUES('10007', 'CAN', 5000, '2018-04-14'); --UNIQUE
+--INSERT INTO calisanlar VALUES('10009', 'cem', '', '2018-04-14'); --NOT NULL
+INSERT INTO calisanlar VALUES('', 'osman', 2000, '2018-04-14');
+--INSERT INTO calisanlar VALUES('', 'osman can', 2000, '2018-04-14'); --PRIMARY KEY
+--INSERT INTO calisanlar VALUES( '10002', 'ayse Yılmaz' ,12000, '2018-04-14'); --PRIMARY KEY
+--INSERT INTO calisanlar VALUES( null, 'filiz ' ,12000, '2018-04-14'); -- PRIMARY KEY
+
+alter table calisanlar add constraint tc PRIMARY KEY (id);
+alter table calisanlar add column calisma_saati int;
+select * from calisanlar;
+
+CREATE TABLE isciler as select 
+
+
